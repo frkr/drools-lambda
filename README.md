8e17aeebca2e74fdc7310391a41578e522c20c0b
DaviSM <davimesquita@gmail.com>
2018-06-10 04:56:17 -0300

    lambda start

--- README.md
+++ README.md
@@ -1,29 +1,24 @@
 ### Build
 ```
-clean package shade:shade
+mvn clean package shade:shade
 ```
 
-### Run 
+### Local Run (sam is broken)
 ```
 sam local invoke -e event.json
 ```
 
-# Error
+### Create Function
 ```
-2018-06-10 03:16:09 Invoking com.github.frkr.DroolsHandler::DroolsHandler (java8)
-2018-06-10 03:16:09 Starting new HTTP connection (1): 169.254.169.254
-2018-06-10 03:16:11 Decompressing /home/davi/Desktop/drools-lambda/target/drools-lambda.jar
+aws lambda create-function --function-name DroolsFunction --runtime java8 --role arn:aws:iam::827387554119:role/service-role/microservice --handler com.github.frkr.DroolsHandler --zip-file fileb://`pwd`/target/lambda.jar   
+```
 
-Fetching lambci/lambda:java8 Docker container image......
-2018-06-10 03:16:13 Mounting /tmp/tmpiQQthP as /var/task:ro inside runtime container
-START RequestId: 90a45aa3-9e5b-40e5-89e8-8c22727eb406 Version: $LATEST
-java.lang.ClassNotFoundException: com.github.frkr.DroolsHandler
-        at java.net.URLClassLoader.findClass(URLClassLoader.java:381)
-        at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
-        at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
-        at java.lang.Class.forName0(Native Method)
-        at java.lang.Class.forName(Class.java:348)
+### Update Function
+```
+aws lambda update-function-code --function-name DroolsFunction --zip-file fileb://`pwd`/target/lambda.jar
+```
 
-END RequestId: 90a45aa3-9e5b-40e5-89e8-8c22727eb406
-REPORT RequestId: 90a45aa3-9e5b-40e5-89e8-8c22727eb406  Duration: 5.37 ms       Billed Duration: 100 ms Memory Size: 128 MB Max Memory Used: 4 MB   
+### Invoke
+```
+aws lambda invoke --function-name DroolsFunction --payload '{"teste":"sim"}' output.log
 ```
--- src/main/java/com/github/frkr/DroolsHandler.java
+++ src/main/java/com/github/frkr/DroolsHandler.java
@@ -1,3 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Davi Saranszky Mesquita https://github.com/frkr/drools-lambda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package com.github.frkr;
 
 import com.amazonaws.services.lambda.runtime.Context;
@@ -12,7 +37,14 @@ public class DroolsHandler implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        LambdaLogger log = context.getLogger();
-        log.log("teteste");
+        String greetingString = "Hello ";
+        int letter;
+        while ((letter = inputStream.read()) != -1) {
+            outputStream.write(Character.toUpperCase(letter));
+            greetingString += (char) Character.toUpperCase(letter);
+        }
+        outputStream.write("seu oreioudo".getBytes());
+        LambdaLogger logger = context.getLogger();
+        logger.log(String.format("Log output: Greeting is '%s'\n", greetingString));
     }
 }
